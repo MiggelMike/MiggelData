6d3e13a13ef816455cbc1423f25d83de1feb671d
MiggelMike <mike.schumacher@arcor.de>
2017-07-19 12:19:40 +0200

    Add files via upload

--- UseEclipse.java
+++ UseEclipse.java
@@ -1,26 +1,46 @@
+import java.util.Calendar;
+import java.util.Date;
 
 public class UseEclipse {
-
+	
 	public static void main(String[] args) {
-		int x1 = 0x012;
-		int x2 = 0x12;
-		System.out.println(x1);
-		System.out.println(x2);
-		System.out.println('\u1126');
-		double d1 = 0.;
-		double d2 = 2e3;	
-		byte b1 = (byte)128;
-		System.out.println(b1);
-		int x5 = 23;
-		byte b4 = (byte)x5;
-		System.out.println("X = " + 3 / 3 % 5);
-		int erg =  Integer.MAX_VALUE;
-		System.out.println("X = " + Integer.MAX_VALUE);
-		System.out.println("X = " + (Integer.MAX_VALUE + 1));
-		System.out.println("X = " + 0x80000000);
-		System.out.println("X = " + 0xffffffe0);
-		short s = 1 + (byte)1234567890123L;
-		System.out.println("X = " + s);
-		// https://bitbucket.org/ccjavad/jse_137638.git 
+		Calendar datum1 = Calendar.getInstance();
+		datum1.set(2017, Calendar.JANUARY, 1);
+		Calendar datum2 = Calendar.getInstance();
+		datum2.set(2017, Calendar.FEBRUARY, 1);
+		System.out.println(Tage(datum1,datum2));
+	}
+	
+	static int Tage(Calendar aDatum1, Calendar aDatum2){
+		int mResult = 0;
+		int mJahr1 = aDatum1.get(Calendar.YEAR);
+		int mJahr2 = aDatum2.get(Calendar.YEAR);
+		for (int jahr = mJahr1; jahr < mJahr2; jahr++) {
+			int mVonMonat = jahr == mJahr1 ? aDatum1.get(Calendar.MONTH) : Calendar.JANUARY; 
+			int mBisMonat = jahr == mJahr2 ? aDatum2.get(Calendar.MONTH) : Calendar.DECEMBER;
+			for (int monat = mVonMonat; monat < mBisMonat; monat++) {
+				int mTage = 31;
+				switch(monat){
+				    case Calendar.APRIL:				    	
+				    case Calendar.JUNE:
+				    case Calendar.SEPTEMBER:
+				    case Calendar.NOVEMBER:
+				    	mTage = 30;
+						break;
+					
+				    case Calendar.FEBRUARY:
+				    	mTage = 28;
+				    	if(IsLeapYear(jahr))
+				    		mTage++;
+				    	break;
+				}
+				mResult += mTage;
+			}	
+		}
+		return mResult;
+	}
+	
+	static boolean IsLeapYear(int aYear){
+		return ((aYear % 4 == 0) && (aYear % 100 != 0) || (aYear % 400 == 0));
 	}
 }
